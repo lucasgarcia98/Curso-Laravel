d0e2b53da5b05b13660edd9825abeb36bf3e9e50
Lucas Garcia <lucks.garcialg@gmail.com>
2021-09-25 20:32:49 -0300

    Criacao do repositorio

---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.main')
+@section('title', 'Criar Evento')
+@section('content')
+
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+        <h1>Crie o seu evento</h1>
+        <form action="/events" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="image">Imagem do Evento:</label>
+                <input type="file" class="form-control-file" id="image" name="image">
+            </div>
+            <div class="form-group">
+                <label for="title">Evento:</label>
+                <input type="text" name="title" id="title" class="form-control" id="title" placeholder="Nome do evento">
+            </div>
+            <div class="form-group">
+                <label for="city">Cidade:</label>
+                <input type="text" name="city" id="city" class="form-control" id="city" placeholder="Nome do evento">
+            </div>
+            <div class="form-group">
+                <label for="title">O evento é privado?</label>
+                <select class="form-control" id="private" name="private" placeholder="Nome do evento">
+                    <option value="0">Nao</option>
+                    <option value="1">Sim</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="title">Descrição:</label>
+                <textarea name="description" class="form-control" id="description" placeholder="O que vai acontecer no evento?"></textarea>
+            </div>
+            <input type="submit" class="btn btn-primary" value="Criar Evento">
+        </form>
+    </div>
+
+@endsection
