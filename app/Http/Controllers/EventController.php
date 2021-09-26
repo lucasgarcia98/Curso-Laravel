b535badad6014d18a3224774ca0edbf1e41704d6
Lucas Garcia <lucks.garcialg@gmail.com>
2021-09-25 23:05:56 -0300

    aula 21

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,8 +10,17 @@ class EventController extends Controller
 {
     public function index()
     {
-        $events = Event::all();
-        return view('welcome', ['events' => $events]);
+        $search = request('search');
+
+        if ($search) {
+            $events = Event::where([
+                ['title', 'like', '%' . $search . '%']
+            ])->get();
+        } else {
+            $events = Event::all();
+        }
+
+        return view('welcome', ['events' => $events, 'search' => $search]);
     }
 
     public function create()
@@ -28,7 +37,7 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
-        $event->items = $request->items;        
+        $event->items = $request->items;
 
         //Image Upload
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
@@ -43,10 +52,10 @@ class EventController extends Controller
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
-    public function show($id){
+    public function show($id)
+    {
         $event = Event::findOrFail($id);
 
         return view('events.show', ['event' => $event]);
     }
 }
-
