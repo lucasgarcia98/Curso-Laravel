df4846dac592ef0737aaf9a393720fb50a98c229
Lucas Garcia <lucks.garcialg@gmail.com>
2021-09-27 20:20:32 -0300

    Aula 30 Final

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -59,10 +59,19 @@ class EventController extends Controller
     public function show($id)
     {
         $event = Event::findOrFail($id);
-
+        $user = auth()->user();
+        $hasUserJoined = false;
+        if($user){
+            $userEvents = $user->eventsAsParticipant->toArray();
+            foreach($userEvents as $userEvent){
+                if($userEvent['id'] == $id){
+                    $hasUserJoined = true;
+                }
+            }
+        }
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
     }
 
     public function dashboard()
@@ -88,10 +97,10 @@ class EventController extends Controller
     {
         $user = auth()->user();
         $event = Event::findOrFail($id);
-        if($user->id != $event->user_id){
+        if ($user->id != $event->user_id) {
             return redirect('/dashboard');
         }
-        
+
         return view('events.edit', ['event' => $event]);
     }
 
@@ -117,5 +126,13 @@ class EventController extends Controller
         $user->eventsAsParticipant()->attach($id);
         $event = Event::findOrFail($id);
         return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+    }
+
+    public function leaveEvent($id)
+    {
+        $user = auth()->user();
+        $user->eventsAsParticipant()->detach($id);
+        $event = Event::findOrFail($id);
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento: ' . $event->title);
     }
 }
